2f09d688c0f8ef0b8927c041a1021c3de026166c
Gordon <47766574+JDoes@users.noreply.github.com>
2020-06-23 12:29:32 -0700

    Update remove_users.sh

--- remove_users.sh
+++ remove_users.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+######################################
+#Use usernames.txt to remove the users that were created with create_users.sh
+
 cat usernames.txt | while read i; do
         userdel -r "$i"
 done
+
+#####################################
+#Remove the user:password pairs file, named users.txt.
 
 rm users.txt
